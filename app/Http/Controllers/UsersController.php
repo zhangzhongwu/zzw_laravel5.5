68754b52a52def1071676d36999ecfb90ed147c5
zzw <1326056159@qq.com>
2018-09-04 08:42:03 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
-    	return view('users.create');
+        return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|unique:users|max:255',
+    		'password' => 'required|confirmed|min:6'
+    	]);
+    	
+    	$user = User::create([
+
+    		'name' => $request->name,
+    		'email' => $request->email,
+    		'password' => bcrypt($request->password),
+    	]);
+
+    	//存入一条缓存的数据，让它只在下一次的请求内有效时
+    	session()->flash('success', '欢迎,您将在这里开启一段新的旅程~');
+    	return redirect()->route('users.show', [$user]);
     }
 }
